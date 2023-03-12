d4bcca0187ba889b3a345eac302a1fa09b26f819
Changhyun <chang.bae@flatironschool.com>
2023-03-11 16:10:49 -0800

    Tasklister Mini Project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form")
+  form.addEventListener("submit", (e) =>{
+    e.preventDefault()
+    makeTaskDescription(e.target.new_task_description.value)
+    form.reset()
+  })
 });
+function makeTaskDescription(taskdescription){
+  let p = document.createElement("p")
+  let btn = document.createElement("button")
+  btn.textContent = "x"
+  p.textContent = `${taskdescription} `
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector("#list").appendChild(p)
+  btn.addEventListener("click", makeDelete)
+   
+}  
+function makeDelete(e){
+  e.target.parentNode.remove()
+}
